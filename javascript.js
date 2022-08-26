63e59dbb00a484b79c944e40e664f9a474787080
MZ <gaksunmee@naver.com>
2022-08-27 03:27:54 +0900

    함수,지역변수,전역변수

--- javascript.js
+++ javascript.js
@@ -1,33 +1,35 @@
-/* 
-let a = +prompt('a?', '');
+let from = "Ann";
 
-if (a == 0) {
-  alert( 0 );
+function showMessage(from, text) {
+
+    from = '*' + from + '*'; // "from"을 좀 더 멋지게 꾸며줍니다.
+  
+    alert( from + ': ' + text );
+  }
+
+ 
+  showMessage(from, "Hello"); // *Ann*: Hello
+  
+  // 함수는 복사된 값을 사용하기 때문에 바깥의 "from"은 값이 변경되지 않습니다.
+  alert( from ); // Ann
+
+
+
+
+
+
+
+let userName = 'John';
+
+function showMessage() {
+  userName = "Bob"; // (1) 외부 변수를 수정함
+
+  let message = 'Hello, ' + userName;
+  alert(message);
 }
-if (a == 1) {
-  alert( 1 );
-}
 
-if (a == 2 || a == 3) {
-  alert( '2,3' );
-}
+alert( userName ); // 함수 호출 전이므로 John 이 출력됨
 
-위의 if문을 switch문으로 변경해보기
-*/
+showMessage();
 
-let a=+prompt('숫자입력','');
-
-switch(a) {
-    case 0: 
-    alert(0);
-    break;
-    
-    case 1:
-    alert(1);
-    break;
-    
-    case 2:
-    case 3:
-    alert ('2,3');
-    
-    }
+alert( userName ); // 함수에 의해 Bob 으로 값이 바뀜
