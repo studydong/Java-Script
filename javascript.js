04b3ef0df9e9d45586fb58391f45a8454d0efa15
MZ <gaksunmee@naver.com>
2022-08-30 14:33:18 +0900

    화살표 함수 사용하기

--- javascript.js
+++ javascript.js
@@ -1,20 +1,25 @@
-let n = prompt('값을 입력하세요','');
-showPrimes(n);
+/* 
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+  }
+  
+  ask(
+  "동의하십니까?",
+  function() { alert("동의하셨습니다."); },
+  function() { alert("취소 버튼을 누르셨습니다."); }
+  );
+*/
 
-function showPrimes(n) {
-  for(let i=2; i<=n; i++) {
+// 위에 함수를 화살표 함수로 간소화 시키기
 
-    if(isPrime(i))  alert(i);
- }
-
+let ask = (question, yes, no) => {
+  if ( confirm(question) ) yes();
+  else no();
 }
- 
- function isPrime(n) {
- 
-   for(let i=2; i<n; i++) {
 
-    if(n % i == 0) return false;
-
-   }
-   return true;
- }
+ask(
+'동의하십니까?' ,
+() => alert('동의하셨습니다.') ,
+() => alert('취소 버튼을 눌렀습니다.') , 
+);
