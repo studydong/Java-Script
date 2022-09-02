8d18236dccbd70d5b17ce46329c5e5e8b3ce8870
MZ <gaksunmee@naver.com>
2022-09-02 20:30:41 +0900

    퀴즈) 명세서의 올바른 사용법

--- javascript.js
+++ javascript.js
@@ -1,39 +1,59 @@
-describe("pow 함수 테스트", function () {
+/* 문제 1. 
+함수 pow의 테스트 코드를 보고 무엇이 잘못되었나 알아보세요.
 
-  describe("x의 3제곱 테스트", function () {
+문법적인 오류는 없고, 테스트는 문제없이 통과 했다.
 
-    function powTest(x) { // x 값을 인자로 받음
 
-      let result = x * x * x;
+  it("주어진 숫자의 n 제곱", function () {
+    let x = 5;
 
-      it(`${x} 의 3 제곱은 :: ${result}`, function () {
-        assert.equal(pow(x, 3), result);
-      });
+    let result = x;   // 5의 값을 반환 합니다. 
+    assert.equal(pow(x, 1), result);    // 5와 1을 인자로 넘기고 result를 비교 , 오류 x
 
-    }
+    result *= x;  // 25
+    assert.equal(pow(x, 2), result);  // 25 , 2 인자 넘김, 오류 x
 
-    for (let x = 1; x <= 5; x++) {
-      powTest(x);
-    }
-
+    result *= x;  // 125
+    assert.equal(pow(x, 3), result);  // 125, 
   });
 
-  describe("x의 4 제곱 테스트", function () {
+*/
 
-    function powTest(x) { // x 값을 인자로 받음
+/*
+1. 테스트는 문제 없이 통과 되겠지만, 하나의 it 블럭에 여러 개의 유스케이스를 넣었기 때문에
+하나라도 오류가 발생하면, 어디서 오류가 발생되었는지 확인하기 어렵다.
+==> 하나의 it 블럭에는 하나의 테스트만 진행하자
+*/
 
-      let result = x * x * x * x;
+/* 문제 해결  */
 
-      it(`${x} 의 4 제곱은 :: ${result}`, function () {
-        assert.equal(pow(x, 4), result);
-      });
+describe('pow' , function() {
 
-    }
+it("5의 1 제곱", function () {
+  assert.equal( pow(5, 1), 5);
+});
 
-    for (let x = 1; x <= 5; x++) {
-      powTest(x);
-    }
+it.only("5의 2 제곱", function () {
+  assert.equal( pow(5, 2), 25);
+});
 
-  });
+it("n이 음수일때는 nan을 반환한다.", function () {
+  assert.isNaN( pow(5, -6));
+});
 
 });
+
+
+/* 번외 
+it 블럭을 여러 개로 쪼개면, 장점은 
+원하는 블럭만 테스트를 진행하게 할 수도 있습니다.
+
+it.only를 사용한다면 해당 블럭만 테스트 합니다.
+
+it.only("5의 2 제곱", function () {
+  assert.equal( pow(5, 2), 25);
+});
+
+
+*/
+
