77bf6bdea1f2e9ab42a6f094c8b36e5ae1d077ed
MZ <gaksunmee@naver.com>
2022-09-06 13:35:48 +0900

    메서드와 This 사용해서 계산기 만들기

--- javascript.js
+++ javascript.js
@@ -1,28 +1,29 @@
-/* 
-function makeUser() {
-  return {
-    name: "John",
-    ref: this
-  };
-};
+/* `calculator`라는 객체를 만들고 세 메서드를 구현해 봅시다.
 
-let user = makeUser();
-
-alert( user.ref.name ); // undefined
+- `read()`에선 프롬프트 창을 띄우고 더할 값 두 개를 입력받습니다. 입력받은 값은 객체의 프로퍼티에 저장합니다.
+- `sum()`은 저장된 두 값의 합을 반환합니다.
+- `mul()`은 저장된 두 값의 곱을 반환합니다.
 */
 
-// 위에서 user.ref.name로 접근했을 때 정상적으로 "John"을 출력할 수 있게 코드를 수정 해보세요.
 
+let calculator = {
+  num: 0,
+  num2: 0,  
 
-function makeUser() {
-  return {
-    name: "John",
-    ref() {
-      return this;
-    }
-  };
+  read() {
+    this.num = +prompt('값을 입력해주세요 (1)', '');
+    this.num2 = +prompt('값을 입력해주세요 (2)', '');
+  },
+
+  sum() {
+    return this.num + this.num2;
+  },
+
+  mul() {
+    return this.num * this.num2;
+  }
+
 };
 
-let user = makeUser();
-
-alert( user.ref().name ); // John
+calculator.read();
+alert(calculator.mul());
