882ddb60422b5d2260372a1b6b84eefd4e363520
MZ <gaksunmee@naver.com>
2022-09-06 13:09:31 +0900

    This의 정확한 사용법

--- javascript.js
+++ javascript.js
@@ -1,17 +1,28 @@
-let user = {
-  
-  name: "John",
-  sizes: {
-   height: 182,
-   width: 50
-  }
-   
- };
- 
- let clone = _.cloneDeep(user);
- 
- alert( user.sizes === clone.sizes );	// true, 같은 객체입니다
- // 즉, user객체와 clone객체는 sizes객체를 공유합니다.
- 
- user.sizes.width++;			// user.sizes에 프로퍼티 width 값을 변경합니다. (후위증가)
- alert(clone.sizes.width);	// 51, 객체를 공유하기 때문에 clone에서 변경사항을 확인할 수 있습니다.
+/* 
+function makeUser() {
+  return {
+    name: "John",
+    ref: this
+  };
+};
+
+let user = makeUser();
+
+alert( user.ref.name ); // undefined
+*/
+
+// 위에서 user.ref.name로 접근했을 때 정상적으로 "John"을 출력할 수 있게 코드를 수정 해보세요.
+
+
+function makeUser() {
+  return {
+    name: "John",
+    ref() {
+      return this;
+    }
+  };
+};
+
+let user = makeUser();
+
+alert( user.ref().name ); // John
